31a81502fa352115edcf8e4bd42be4bb80b08027
Evgenii Akhmetzianov <erakhmetzyan@yandex-team.ru>
2023-12-25 14:37:53 +0700

    First handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import "net/http"
+
+func updateCounter(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(res, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/update/counter/`, updateCounter)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
