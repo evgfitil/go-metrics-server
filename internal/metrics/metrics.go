f81e71ef39f2e66d75cdb8984d69db72f38f1eda
Evgenii Akhmetzianov <erakhmetzyan@yandex-team.ru>
2023-12-29 21:01:26 +0700

    Add GetValueAsString functions

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -1,7 +1,10 @@
 package metrics
 
+import "fmt"
+
 type Metric interface {
 	GetName() string
+	GetValueAsString() string
 }
 
 type Counter struct {
@@ -13,6 +16,10 @@ func (c Counter) GetName() string {
 	return c.Name
 }
 
+func (c Counter) GetValueAsString() string {
+	return fmt.Sprintf("%d", c.Value)
+}
+
 type Gauge struct {
 	Name  string
 	Value float64
@@ -20,4 +27,8 @@ type Gauge struct {
 
 func (g Gauge) GetName() string {
 	return g.Name
+}
+
+func (g Gauge) GetValueAsString() string {
+	return fmt.Sprintf("%f", g.Value)
 }
