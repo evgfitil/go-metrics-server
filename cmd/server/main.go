305139048d6efd6343067f5fbb1f0c82b2f91c63
Evgenii Akhmetzianov <eraakhmetzyanov@sberbank.ru>
2024-01-17 20:57:56 +0300

    iter6: add zap logger, WithLogging hanlder middleware

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"github.com/evgfitil/go-metrics-server.git/internal/handlers"
+	"github.com/evgfitil/go-metrics-server.git/internal/logger"
 	"github.com/evgfitil/go-metrics-server.git/internal/storage"
 	"github.com/go-chi/chi/v5"
-	"log"
 	"net/http"
 )
 
@@ -19,11 +19,18 @@ func MetricsRouter(s *storage.MemStorage) chi.Router {
 }
 
 func main() {
+	logger.InitLogger()
+	defer logger.Sugar.Sync()
+
 	s := storage.NewMemStorage()
 	config := NewConfig()
 	err := config.ParseFlags()
 	if err != nil {
-		log.Fatalf("error getting arguments: %v", err)
+		logger.Sugar.Fatalf("error getting arguments: %v", err)
 	}
-	http.ListenAndServe(config.bindAddress, MetricsRouter(s))
+	logger.Sugar.Infoln("starting server")
+	err = http.ListenAndServe(config.bindAddress, logger.WithLogging(MetricsRouter(s)))
+	if err != nil {
+		logger.Sugar.Fatalf("error starting server: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -12,6 +12,8 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
