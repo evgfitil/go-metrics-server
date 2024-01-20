3465f03188debd2695829e541e64ceb43b03c5ce
Evgenii Akhmetzianov <eraakhmetzyanov@sberbank.ru>
2024-01-20 11:53:00 +0300

    Iter7: refactor handlers test

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -1,11 +1,14 @@
 package handlers
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/evgfitil/go-metrics-server.git/internal/metrics"
 	"github.com/evgfitil/go-metrics-server.git/internal/storage"
 	"github.com/go-chi/chi/v5"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,10 +16,8 @@ import (
 
 func testMetricsRouter(s *storage.MemStorage) chi.Router {
 	r := chi.NewRouter()
-	r.Get("/value/{type}/{name}", GetMetrics(s))
-	r.Route("/update", func(r chi.Router) {
-		r.Post("/{type}/{name}/{value}", UpdateMetrics(s))
-	})
+	r.Post("/value/", GetMetrics(s))
+	r.Post("/update/", UpdateMetrics(s))
 	return r
 }
 
@@ -31,41 +32,47 @@ func TestGetMetricsHandler(t *testing.T) {
 
 	type want struct {
 		statusCode int
+		body       string
 	}
 	tests := []struct {
 		name          string
 		requestMethod string
 		requestPath   string
+		requestBody   metrics.Metrics
 		want          want
 	}{
 		{
 			name:          "valid request",
-			requestMethod: http.MethodGet,
-			requestPath:   "/value/counter/testCounter",
+			requestMethod: http.MethodPost,
+			requestPath:   "/value/",
+			requestBody:   metrics.Metrics{ID: "testCounter", MType: "counter"},
 			want: want{
 				statusCode: http.StatusOK,
+				body:       `{"id":"testCounter","type":"counter","delta":100}`,
 			},
 		},
 		{
 			name:          "wrong requestMethod",
-			requestMethod: http.MethodPost,
-			requestPath:   "/value/counter/testCounter",
+			requestMethod: http.MethodGet,
+			requestPath:   "/value/",
 			want: want{
 				statusCode: http.StatusMethodNotAllowed,
 			},
 		},
 		{
 			name:          "wrong requestPath",
-			requestMethod: http.MethodGet,
+			requestMethod: http.MethodPost,
 			requestPath:   "/wrong",
+			requestBody:   metrics.Metrics{ID: "testCounter", MType: "counter"},
 			want: want{
 				statusCode: http.StatusNotFound,
 			},
 		},
 		{
 			name:          "get non existing metric",
-			requestMethod: http.MethodGet,
-			requestPath:   "/value/counter/nonExistingMetric",
+			requestMethod: http.MethodPost,
+			requestPath:   "/value/",
+			requestBody:   metrics.Metrics{ID: "testTest", MType: "counter"},
 			want: want{
 				statusCode: http.StatusNotFound,
 			},
@@ -74,14 +81,20 @@ func TestGetMetricsHandler(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			req, err := http.NewRequest(tt.requestMethod, ts.URL+tt.requestPath, nil)
+			jsonBody, _ := json.Marshal(tt.requestBody)
+			req, err := http.NewRequest(tt.requestMethod, ts.URL+tt.requestPath, bytes.NewBuffer(jsonBody))
+			req.Header.Set("Content-Type", "application/json")
 			require.NoError(t, err)
 
 			resp, err := ts.Client().Do(req)
 			require.NoError(t, err)
 			defer resp.Body.Close()
 
+			body, _ := io.ReadAll(resp.Body)
 			assert.Equal(t, tt.want.statusCode, resp.StatusCode)
+			if tt.want.statusCode == http.StatusOK {
+				assert.JSONEq(t, tt.want.body, string(body))
+			}
 		})
 	}
 }
@@ -91,36 +104,47 @@ func TestUpdateMetricsHandler(t *testing.T) {
 
 	ts := httptest.NewServer(testMetricsRouter(mockStorage))
 	defer ts.Close()
+	validCounterValue := int64(100)
+	validGaugeValue := 123.12
+	validCounterMetric := metrics.Metrics{ID: "testCounter", MType: "counter", Delta: &validCounterValue}
+	validGaugeMetric := metrics.Metrics{ID: "testGauge", MType: "gauge", Value: &validGaugeValue}
+	invalidMetric := metrics.Metrics{ID: "testInvalid", MType: "someType", Value: &validGaugeValue}
 
 	type want struct {
 		statusCode int
+		body       string
 	}
 	tests := []struct {
 		name          string
 		requestMethod string
 		requestPath   string
+		requestBody   metrics.Metrics
 		want          want
 	}{
 		{
 			name:          "valid Counter update",
 			requestMethod: http.MethodPost,
-			requestPath:   "/update/counter/testCounter/123",
+			requestPath:   "/update/",
+			requestBody:   validCounterMetric,
 			want: want{
 				statusCode: http.StatusOK,
+				body:       `{"id":"testCounter","type":"counter","delta":100}`,
 			},
 		},
 		{
 			name:          "valid Gauge update",
 			requestMethod: http.MethodPost,
-			requestPath:   "/update/gauge/testGauge/123.12",
+			requestPath:   "/update/",
+			requestBody:   validGaugeMetric,
 			want: want{
 				statusCode: http.StatusOK,
+				body:       `{"id":"testGauge","type":"gauge","value":123.12}`,
 			},
 		},
 		{
 			name:          "invalid request method",
 			requestMethod: http.MethodGet,
-			requestPath:   "/update/gauge/testGauge/123.12",
+			requestPath:   "/update/",
 			want: want{
 				statusCode: http.StatusMethodNotAllowed,
 			},
@@ -128,7 +152,8 @@ func TestUpdateMetricsHandler(t *testing.T) {
 		{
 			name:          "invalid path",
 			requestMethod: http.MethodPost,
-			requestPath:   "/update/gauge/test/Gauge/123.12",
+			requestPath:   "/update/u",
+			requestBody:   validGaugeMetric,
 			want: want{
 				statusCode: http.StatusNotFound,
 			},
@@ -136,15 +161,8 @@ func TestUpdateMetricsHandler(t *testing.T) {
 		{
 			name:          "invalid metric type",
 			requestMethod: http.MethodPost,
-			requestPath:   "/update/histogram/testHistogram/123.12",
-			want: want{
-				statusCode: http.StatusBadRequest,
-			},
-		},
-		{
-			name:          "invalid Counter value",
-			requestMethod: http.MethodPost,
-			requestPath:   "/update/counter/testCounter/123.12",
+			requestPath:   "/update/",
+			requestBody:   invalidMetric,
 			want: want{
 				statusCode: http.StatusBadRequest,
 			},
@@ -153,14 +171,20 @@ func TestUpdateMetricsHandler(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			req, err := http.NewRequest(tt.requestMethod, ts.URL+tt.requestPath, nil)
+			jsonBody, _ := json.Marshal(tt.requestBody)
+			req, err := http.NewRequest(tt.requestMethod, ts.URL+tt.requestPath, bytes.NewBuffer(jsonBody))
+			req.Header.Set("Content-Type", "application/json")
 			require.NoError(t, err)
 
 			resp, err := ts.Client().Do(req)
 			require.NoError(t, err)
 			defer resp.Body.Close()
 
+			body, _ := io.ReadAll(resp.Body)
 			assert.Equal(t, tt.want.statusCode, resp.StatusCode)
+			if tt.want.statusCode == http.StatusOK {
+				assert.JSONEq(t, tt.want.body, string(body))
+			}
 		})
 	}
 }
