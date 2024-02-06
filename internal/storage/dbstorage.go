a655096f97d70d84a5dd1659cc5b2b204e762a38
Evgenii Akhmetzianov <erakhmetzyan@yandex-team.ru>
2024-02-06 21:47:51 +0300

    Iter11: add update operations for gauge and counter

--- internal/storage/dbstorage.go
+++ internal/storage/dbstorage.go
@@ -32,16 +32,41 @@ func (db *DBStorage) Ping(ctx context.Context) error {
 func (db *DBStorage) Update(ctx context.Context, metric *metrics.Metrics) {
 	switch metric.MType {
 	case "counter":
-		db.updateCounter(ctx, metric)
+		err := db.updateCounter(ctx, metric)
+		if err != nil {
+			logger.Sugar.Errorf("error updating counter metric: %v", err)
+		}
 	case "gauge":
-		db.updateGauge(ctx, metric)
+		err := db.updateGauge(ctx, metric)
+		if err != nil {
+			logger.Sugar.Errorf("error updating gauge metric: %v", err)
+		}
 	}
 }
 
-func (db *DBStorage) updateCounter(ctx context.Context, metric *metrics.Metrics) {
+func (db *DBStorage) updateCounter(ctx context.Context, metric *metrics.Metrics) error {
+	var currentDelta *int64
+
+	row := db.connPool.QueryRowContext(ctx, "SELECT delta FROM counter WHERE id = $1", metric.ID)
+	err := row.Scan(&currentDelta)
+	if err != nil && err != sql.ErrNoRows {
+		return err
+	}
+	if currentDelta != nil {
+		*currentDelta += *metric.Delta
+	} else {
+		currentDelta = metric.Delta
+	}
+
+	_, err = db.connPool.ExecContext(ctx,
+		"INSERT INTO counter (id, delta) VALUES ($1, $2) ON CONFLICT (id) DO UPDATE SET delta = $2", metric.ID, *currentDelta)
+	return err
 }
 
-func (db *DBStorage) updateGauge(ctx context.Context, metric *metrics.Metrics) {
+func (db *DBStorage) updateGauge(ctx context.Context, metric *metrics.Metrics) error {
+	_, err := db.connPool.ExecContext(ctx,
+		"INSERT INTO gauge (id, value) VALUES ($1, $2) ON CONFLICT (id) DO UPDATE SET value = $2", metric.ID, metric.Value)
+	return err
 }
 
 func (db *DBStorage) Get(ctx context.Context, metricName string) (*metrics.Metrics, bool) {
